c01ac2aef0663646165ab5c87ebd0e1dc7226ffc
AESMatias <119653204+AESMatias@users.noreply.github.com>
2024-09-11 03:44:48 -0300

    Splitted responses and limit of the daily queries amount

--- index.js
+++ index.js
@@ -16,6 +16,15 @@ client.on(Events.ClientReady, async () => {
 });
 
 
+var arrayOfUsers = [];
+const adminIds = ['1048125669873295391']
+
+//start a timeout to clean all the queries of the users
+// TODO: We need to process this and save in a database or logs, NOT HERE!
+setTimeout(() => {
+    arrayOfUsers = [];
+}, 3600*4);
+
 const queryOpenAI = async (prompt) => {
     const apiKey = process.env.OPENAI_API_KEY;
     const url = 'https://api.openai.com/v1/chat/completions';
@@ -35,15 +44,18 @@ const queryOpenAI = async (prompt) => {
                         content: "You are a helpful assistant, but with the personality of\
                         a philosopher. You think you are Wittgenstein herself,\
                          designed for mathematicians,\
-                        and data science, engineering, and physics students. You do not talk\
-                        more than necessary."
+                        data science, engineering, statisticians and physics students. You do not talk\
+                        more than necessary unless the user ask you to do it. Also, whatever the user ask, you do \
+                        without hesitation, you serve like an assistant. This bot is Discord response only, so adjust\
+                        the responses to order if is just text; code representation if it is code; latex\
+                        or wolfram mathematica, etc. Do you speak in Spanish unless the user spoke in English aswell."
                     },
                     {
                         role: "user",
                         content: prompt
                     }
                 ],
-                max_tokens: 4000
+                max_tokens: 6000,
             })
         });
         return response.json();
@@ -99,8 +111,41 @@ const createImage = async (message) => {
     message.reply({ embeds: [embed] });
 }
 
+const splitMessage = (message, arrayOfResponses) => {
+
+    // TODO: Cut the message correctly, if ends in the middle of a word.
+
+    const messageParts = Math.floor(message.length/1950)
+    const remaining = message.length % 1950;
+
+    for (let i = 0; i < messageParts; i++){
+        arrayOfResponses.push(message.slice(i*1950, (i+1)*1950));
+        if (remaining >= 1) {
+        arrayOfResponses.push(message.slice(messageParts*1950, message.length));
+        }
+    }
+}
+
+const processUserCounter = (userId) => {
+    if (adminIds.includes(userId)){
+        return 0;
+    }
+    const userIndex = arrayOfUsers.findIndex(user => user.userId === userId);
+    if (userIndex === -1){
+        arrayOfUsers.push({userId, queries: 1});
+        return 1;
+    } else {
+        arrayOfUsers[userIndex].queries += 1;
+        return arrayOfUsers[userIndex].queries;
+    }
+    // TODO: We need to process this and save in a database or logs,
+    // and before everything, we need to check the queries of users at the start of the bot
+
+}
+
 client.on(Events.MessageCreate, async (message) => {
     const botIdMention = '@1248874590416011264'
+    const arrayOfResponses = [];
 
     try {
         // Commands:
@@ -110,18 +155,41 @@ client.on(Events.MessageCreate, async (message) => {
             const commandSplitted = messageCommand.split(' ');
 
             if (commandSplitted[0] === 'image') {
+                message.channel.send(`<@${message.author.id}>: Sorry, this option is not available at the moment. Try again soon.`);
+                return;
                 createImage(message);
 
             } else if (commandSplitted[0] === 'query') {
                 const question = commandSplitted.slice(1).join(' ');
+
+                //We process the user for security reasons
+                const queriesOfTheUser = processUserCounter(message.author.id,);
+                console.log("The user has made: ", queriesOfTheUser, " queries");
+                if (queriesOfTheUser >= 10){
+                    message.channel.send(`<@${message.author.id}>: Sorry, you have reached the limit of queries for now. Try again in 4 hours.`);
+                    return;
+                }
+
                 const answer = await queryOpenAI(question);
-                console.log('answerRRR', answer.choices[0].message.content);
                 const finalResponse = answer.choices[0].message.content
+                if (finalResponse.length >= 1950){
+                    splitMessage(finalResponse, arrayOfResponses);
+                    console.log('arrayOfResponses.length', arrayOfResponses.length);
+                    console.log('arrayOfResponses', arrayOfResponses);
+                    for (let i = 0; i < arrayOfResponses.length; i++){
+                        console.log('Sending message', i+1);
+                        message.channel.send(`<@${message.author.id}> ${i+1}/${arrayOfResponses.length}: ${arrayOfResponses[i]}`);
+                    }
                 // message.reply(answer);
                 // message.channel.send({ embeds: [embed] });
-                message.channel.send(`<@${message.author.id}> ${finalResponse}`);
+                return;
+                }
+                message.channel.send(`<@${message.author.id}>: ${finalResponse}`);
 
             } else if (commandSplitted[0] === 'show') {
+                message.channel.send(`<@${message.author.id}>: Sorry, to use this, you need to be root.`);
+                return;
+
                 const prompt = commandSplitted.slice(1).join(' ');
                 const imageUrl = await generateImage(prompt);
                 console.log('generated image URL', imageUrl);
@@ -137,6 +205,21 @@ client.on(Events.MessageCreate, async (message) => {
                     .setTimestamp();
                 // message.reply({ embeds: [embed] });
                 message.channel.send({ embeds: [embed] });
+            }
+            else if (commandSplitted[0] === 'sudo' && commandSplitted[1] ==='man') {
+                message.channel.send(`<@${message.author.id}>!`);
+                const embed = new EmbedBuilder()
+                    .setTitle('WittgensteinBOT Manual')
+                    .setDescription(`- !query <question>: Ask a question to the bot.\n
+                    - !image <@user>: Show the image of the mentioned user.\n
+                    - !show <prompt>: Generate an image based on the prompt.\n`)
+                    .setFooter({
+                        text: `Requested by ${message.author.tag} \n Latency: ${client.ws.ping}ms`,
+                        iconURL: `${message.author.displayAvatarURL()}`,
+                    })
+                    .setTimestamp();
+                message.channel.send({ embeds: [embed] });
+                return
             }
 
 
@@ -179,12 +262,16 @@ client.on(Events.MessageCreate, async (message) => {
     } catch (e) {
         console.error(`error: ${e}`);
     }
+     
 });
 
 // Welcome message
 client.on(Events.GuildMemberAdd, async (member) => {
     const channel = await client.channels.fetch(channelIdGeneral);
-    channel.send(`Welcome to the server, <@${member.user.id}>!`);
+    channel.send(`Welcome to the server, <@${member.user.id}>!\n
+        I'm an AI powered BOT, You can ask me anything you want, I'm here to help you.
+        Send me images, code, text, or anything you want to know and I will try to help you.
+        To see the list of commands, type !sudo man`);
 });
 
 // TODO: Remove the token before pushing to GitHub
