3f382f49c2f7cad5868264b4839952739a9b94b1
AESMatias <119653204+AESMatias@users.noreply.github.com>
2024-09-11 18:23:01 -0300

    Adding PM2 config and read.md file

---
+++ config/ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+    {
+      name: 'Wittgenstein BOT',
+      cwd: './', //The current working directory for the application
+      script: 'index.js',
+      output: './logs/WittgensteinBOT-out.log',
+      error: './logs/WittgensteinBOT-err.log',
+      log: './logs/WittgensteinBOT-combined.log',
+      time: true,
+      watch: true,
+      ignore_watch: ['node_modules', 'logs'],
+    },
+  ],
+};
